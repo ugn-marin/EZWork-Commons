724d26ed0b35c6a158d753595e23790bd4a33f67
Eugene Marin <em88il@gmail.com>
2022-06-26 02:03:58 +0300

    Env interface

--- src/ezw/runtime/Environment.java
+++ src/ezw/runtime/Environment.java
@@ -3,11 +3,9 @@ package ezw.runtime;
 /**
  * Common environment properties.
  */
-public abstract class Environment {
-    public static final String userName = System.getProperty("user.name");
-    public static final String userHome = System.getProperty("user.home");
-    public static final String workingDir = System.getProperty("user.dir");
-    public static final String tempDir = System.getProperty("java.io.tmpdir");
-
-    private Environment() {}
+public interface Environment {
+    String userName = System.getProperty("user.name");
+    String userHome = System.getProperty("user.home");
+    String workingDir = System.getProperty("user.dir");
+    String tempDir = System.getProperty("java.io.tmpdir");
 }
